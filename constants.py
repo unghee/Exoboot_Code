3430db077d963dd18e04e1a94ee76e477e1e513f
vashetty <vashetty@umich.edu>
2022-04-21 19:06:38 -0400

    After recalibration

--- constants.py
+++ constants.py
@@ -3,85 +3,116 @@ import numpy as np
 from enum import Enum
 from typing import Type
 
+
 class ExobootModel(Enum):
     EB45 = 0
     EB51 = 1
 
-ExoType : Type[ExobootModel] = ExobootModel.EB51
+
+ExoType: Type[ExobootModel] = ExobootModel.EB51
 #ExoType = Type[ExobootModel] = ExobootModel.EB45
 
 DEFAULT_BAUD_RATE = 230400
 TARGET_FREQ = 200
-if(ExoType == ExobootModel.EB45):
-  #EB-45
-  MAX_ANKLE_ANGLE = 90 ## degrees, plantarflexion
-  MIN_ANKLE_ANGLE = -45 # degrees, dorsiflexion
+if (ExoType == ExobootModel.EB45):
+    #EB-45
+    MAX_ANKLE_ANGLE = 90  ## degrees, plantarflexion
+    MIN_ANKLE_ANGLE = -45  # degrees, dorsiflexion
 
-  # These polynomials are derived from the calibration routine (calibrate.py), analyzed with transmission_analysis.py
-  LEFT_ANKLE_TO_MOTOR = [ 1.42930145e-05,  9.60847697e-04,  9.66033271e-03,  1.21997272e+00,
-  -7.39600859e+02, -2.96580581e+04]
-  RIGHT_ANKLE_TO_MOTOR = [-1.02595964e-05, -9.28352253e-04, -3.10556810e-02, -1.40767218e+00,
-  7.57989378e+02, -3.48608044e+03]
+    # These polynomials are derived from the calibration routine (calibrate.py), analyzed with transmission_analysis.py
+    LEFT_ANKLE_TO_MOTOR = [
+        1.42930145e-05, 9.60847697e-04, 9.66033271e-03, 1.21997272e+00,
+        -7.39600859e+02, -2.96580581e+04
+    ]
+    RIGHT_ANKLE_TO_MOTOR = [
+        -1.02595964e-05, -9.28352253e-04, -3.10556810e-02, -1.40767218e+00,
+        7.57989378e+02, -3.48608044e+03
+    ]
 
-  # These points are used to create a Pchip spline, which defines the transmission ratio as a function of ankle angle
-  ANKLE_PTS_LEFT = np.array([-40, -20, 0, 10, 20, 30, 40, 45.6, 50, 55])  # Deg
-  TR_PTS_LEFT = np.array([19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5, -11 ])  # Nm/Nm
+    # These points are used to create a Pchip spline, which defines the transmission ratio as a function of ankle angle
+    ANKLE_PTS_LEFT = np.array([-40, -20, 0, 10, 20, 30, 40, 45.6, 50,
+                               55])  # Deg
+    TR_PTS_LEFT = np.array([19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5,
+                            -11])  # Nm/Nm
 
-  ANKLE_PTS_RIGHT = np.array([-40, -20, 0, 10, 20, 30, 40, 45.6, 50, 55])  # Deg
-  TR_PTS_RIGHT = np.array([19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5, -11 ])  # Nm/Nm
+    ANKLE_PTS_RIGHT = np.array([-40, -20, 0, 10, 20, 30, 40, 45.6, 50,
+                                55])  # Deg
+    TR_PTS_RIGHT = np.array([19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5,
+                             -11])  # Nm/Nm
 
-  LEFT_ANKLE_ANGLE_OFFSET = 201#-92  # 7,
-  RIGHT_ANKLE_ANGLE_OFFSET = -150#-200# deg
- 
-elif(ExoType == ExobootModel.EB51):
+    LEFT_ANKLE_ANGLE_OFFSET = 201  #-92  # 7,
+    RIGHT_ANKLE_ANGLE_OFFSET = -150  #-200# deg
 
-  #EB-51
-  MAX_ANKLE_ANGLE = 130 
-  MIN_ANKLE_ANGLE = -95
+elif (ExoType == ExobootModel.EB51):
 
-  #EB-51
-  LEFT_ANKLE_TO_MOTOR = np.array([-2.32052284e-06,  6.29683551e-05,  4.45431399e-02,  3.09556691e+00,
-  -5.49941515e+02, -1.11211736e+04])
-  RIGHT_ANKLE_TO_MOTOR = np.array([ 5.37977952e-06, -2.76399833e-04, -7.58092528e-02, -1.65003833e+00,
-    6.38860770e+02,  9.21647151e+03])
+    #EB-51
+    MAX_ANKLE_ANGLE = 130
+    MIN_ANKLE_ANGLE = -95
 
-  #EB-51
-  ANKLE_PTS_LEFT = np.array([-67, -60, -40, -20, -10 ,0, 10, 20, 30, 40, 45.6, 55, 80, 90, 100])
-  TR_PTS_LEFT = np.array([14.85, 14, 13.8, 13.7, 13.16, 12, 10.43, 8, 5.5, 2.3, 0.4, -3.3, -10, -11.30, -10.95])
-  
-  
-  ANKLE_PTS_RIGHT = np.array([-67, -60, -50, -40, -20, -10 ,0, 10, 20, 30, 40, 45.6, 55, 80, 90])
-  TR_PTS_RIGHT = np.array([15.5, 13.35, 11.95, 12.03, 13.82, 14.3, 13.96, 12.77, 10.56, 7.1, 3.19, 0.4, -3.78, -11.94, -12.3])
+    #EB-51
+    LEFT_ANKLE_TO_MOTOR = np.array([
+        -1.39600219e-06, -1.19669863e-04, 5.18587784e-02, 3.48021800e+00,
+        -5.71389166e+02, -2.84883844e+04
+    ])
+    #LEFT_ANKLE_TO_MOTOR = np.array([-2.32052284e-06, 6.29683551e-05, 4.45431399e-02, 3.09556691e+00,-5.49941515e+02, -1.11211736e+04])  #used for the test trials
 
+    RIGHT_ANKLE_TO_MOTOR = np.array([
+        5.05377935e-06, -2.83727629e-04, -7.31477078e-02, -1.56178494e+00,
+        6.34368537e+02, 9.35489733e+03
+    ])
+    #RIGHT_ANKLE_TO_MOTOR = np.array([ 5.37977952e-06, -2.76399833e-04, -7.58092528e-02, -1.65003833e+00,6.38860770e+02,  9.21647151e+03]) #used for the test trials
 
-  # TODO: attempt to change the manual picking of ANKLE_PTS and TR_PTS
-  # LEFT_ANKLE_TO_TR = np.array([ 5.00380707e-07,  8.20159320e-05,  2.53334311e-03,  5.29066571e-02,
-  #  -1.64252746e+01])
-  # RIGHT_ANKLE_TO_TR = np.array([ 4.83188447e-07, -3.83712114e-05, -3.61934700e-03,  4.54812251e-01,
-  #        -2.89416189e+01]) ## NEED TO CHANGE FOR RIGHT ANKLE
+    #EB-51
+    ANKLE_PTS_LEFT = np.array([
+        -67, -60, -47, -40, -20, -10, 0, 10, 20, 30, 40, 45.6, 55, 80, 90, 100,
+        112
+    ])
+    TR_PTS_LEFT = np.array([
+        14.85, 14, 11.89, 12.74, 14.28, 13.71, 12.54, 10.43, 8, 5.5, 2.3, 0.4,
+        -3.3, -10, -11.30, -10.95, -8.75
+    ])
 
-  #EB-51
-  LEFT_ANKLE_ANGLE_OFFSET = -67  # deg
-  RIGHT_ANKLE_ANGLE_OFFSET = 87.1 #100  # deg
+    #ANKLE_PTS_LEFT = np.array([-67, -60, -40, -20, -10 ,0, 10, 20, 30, 40, 45.6, 55, 80, 90, 100])  #used for the test trials
+    #TR_PTS_LEFT = np.array([14.85, 14, 13.8, 13.7, 13.16, 12, 10.43, 8, 5.5, 2.3, 0.4, -3.3, -10, -11.30, -10.95])  #used for the test trials
+
+    ANKLE_PTS_RIGHT = np.array([
+        -67, -60, -50, -40, -20, -10, 0, 10, 20, 30, 40, 45.6, 55, 70, 80, 86
+    ])
+    TR_PTS_RIGHT = np.array([
+        15.5, 13.35, 11.95, 12.03, 13.82, 14.3, 13.96, 12.77, 10.56, 7.1, 3.19,
+        0.66, -3.78, -9.55, -12.58, -13.09
+    ])
+    #ANKLE_PTS_RIGHT = np.array([-67, -60, -50, -40, -20, -10 ,0, 10, 20, 30, 40, 45.6, 55, 80, 90]) #used for the test trials
+    #TR_PTS_RIGHT = np.array([15.5, 13.35, 11.95, 12.03, 13.82, 14.3, 13.96, 12.77, 10.56, 7.1, 3.19, 0.4, -3.78, -11.94, -12.3])#used for the test trials
+
+    # TODO: attempt to change the manual picking of ANKLE_PTS and TR_PTS
+    # LEFT_ANKLE_TO_TR = np.array([ 5.00380707e-07,  8.20159320e-05,  2.53334311e-03,  5.29066571e-02,
+    #  -1.64252746e+01])
+    # RIGHT_ANKLE_TO_TR = np.array([ 4.83188447e-07, -3.83712114e-05, -3.61934700e-03,  4.54812251e-01,
+    #        -2.89416189e+01]) ## NEED TO CHANGE FOR RIGHT ANKLE
+
+    #EB-51
+    LEFT_ANKLE_ANGLE_OFFSET = -67  # deg
+    RIGHT_ANKLE_ANGLE_OFFSET = 87.1  #100  # deg
 
 # Add to these lists if dev_ids change, or new exos or actpacks are purchased!
 
-RIGHT_EXO_DEV_IDS = [77, 17584] #for EB-51
+RIGHT_EXO_DEV_IDS = [77, 17584]  #for EB-51
 
-LEFT_EXO_DEV_IDS = [888, 48390] # for EB-51
+LEFT_EXO_DEV_IDS = [888, 48390]  # for EB-51
 MS_TO_SECONDS = 0.001
 # Converts raw Dephy encoder output to degrees
 K_TO_NM_PER_DEGREE = 0  #
 B_TO_NM_PER_DEGREE_PER_S = 0
 
-ENC_CLICKS_TO_DEG = 1/(2**14/360)
+ENC_CLICKS_TO_DEG = 1 / (2**14 / 360)
 # Getting the motor current to motor torque conversion is tricky, and any updates to
 # the firmware should confirm that Dephy has not changed their internal current units.
 # This constant assumes we are using q-axis current.
 MOTOR_CURRENT_TO_MOTOR_TORQUE = 0.000146  # mA to Nm
 
 # Dephy units to deg/s --experimentally estimated because their numbers are wrong
-DEPHY_VEL_TO_MOTOR_VEL = 0.025*ENC_CLICKS_TO_DEG
+DEPHY_VEL_TO_MOTOR_VEL = 0.025 * ENC_CLICKS_TO_DEG
 
 # ankle_torque ~ AVERAGE_TRANSMISSION_RATIO*motor_torque
 AVERAGE_TRANSMISSION_RATIO = 14  # Used to roughly map motor to ankle impedance
--- transmission_analysis_modified.py
+++ transmission_analysis_modified.py
@@ -6,34 +6,31 @@ from scipy import signal
 from scipy import interpolate
 import constants
 
-
 folder = 'exo_data/'
 # for filename in ["20220210_1440_calibration2_LEFT.csv"]:
 # for filename in ["20220216_1229_calibration2_LEFT.csv"]:
-for filename in ["20220216_1332_calibration2_LEFT.csv"]:
+for filename in ["1_RIGHT.csv"]:
     with open(folder + filename) as f:
-        motor_angle = [int(row["motor_angle"])
-                       for row in csv.DictReader(f)]
+        motor_angle = [int(row["motor_angle"]) for row in csv.DictReader(f)]
     with open(folder + filename) as f:
-        ankle_angle = [np.floor(float(row["ankle_angle"]))
-                       for row in csv.DictReader(f)]
-    motor_angle = np.array(motor_angle)*constants.ENC_CLICKS_TO_DEG
+        ankle_angle = [
+            np.floor(float(row["ankle_angle"])) for row in csv.DictReader(f)
+        ]
+    motor_angle = np.array(motor_angle) * constants.ENC_CLICKS_TO_DEG
 
     # Sort the data points
     zipped_sorted_lists = sorted(zip(ankle_angle, motor_angle))
     mytuples = zip(*zipped_sorted_lists)
-    ankle_angle, motor_angle = [
-        list(mytuple) for mytuple in mytuples]
-   # Filter
+    ankle_angle, motor_angle = [list(mytuple) for mytuple in mytuples]
+    # Filter
     b, a = signal.butter(N=1, Wn=0.05)
-    motor_angle = signal.filtfilt(
-        b, a, motor_angle, method="gust")
-    ankle_angle = signal.filtfilt(
-        b, a, ankle_angle, method="gust")
+    motor_angle = signal.filtfilt(b, a, motor_angle, method="gust")
+    ankle_angle = signal.filtfilt(b, a, ankle_angle, method="gust")
 
-   # Polyfit
-    p = np.polyfit(ankle_angle, motor_angle /
-                   constants.ENC_CLICKS_TO_DEG, deg=5)
+    # Polyfit
+    p = np.polyfit(ankle_angle,
+                   motor_angle / constants.ENC_CLICKS_TO_DEG,
+                   deg=5)
     print('Polynomial coefficients: ', p)
 
     p_deg = np.polyfit(ankle_angle, motor_angle, deg=5)
@@ -47,8 +44,6 @@ for filename in ["20220216_1332_calibration2_LEFT.csv"]:
     plt.plot(ankle_angle, motor_angle)
     plt.plot(ankle_angle, polyfitted_left_motor_angle, linestyle='dashed')
 
-   
-
     plt.figure(2)
     p_deriv = np.polyder(p_deg)
     print('Polynomial deriv coefficients: ', p_deriv)
@@ -57,29 +52,42 @@ for filename in ["20220216_1332_calibration2_LEFT.csv"]:
     plt.plot(ankle_angle, -TR_from_polyfit, label="polyfit")
 
     TR_from_ankle_angle = interpolate.PchipInterpolator(
-                ankle_angle, TR_from_polyfit)
+        ankle_angle, TR_from_polyfit)
 
-    plt.plot(ankle_angle, -TR_from_ankle_angle(ankle_angle), linewidth=5, label = "pchip auto")
+    plt.plot(ankle_angle,
+             TR_from_ankle_angle(ankle_angle),
+             linewidth=5,
+             label="pchip auto"
+             )  #Multiply -1 to TR_from-ankle_angle(ankle_angle) for Left
 
-    ankle_pts = [-40, -20, 0, 10, 20, 30, 40, 45.6, 50, 55]
-    deriv_pts =  [19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5, -11 ]
+    #RIGHT
+    ankle_pts = np.array([
+        -67, -60, -50, -40, -20, -10, 0, 10, 20, 30, 40, 45.6, 55, 70, 80, 86
+    ])
+    deriv_pts = np.array([
+        15.5, 13.35, 11.95, 12.03, 13.82, 14.3, 13.96, 12.77, 10.56, 7.1, 3.19,
+        0.66, -3.78, -9.55, -12.58, -13.09
+    ])
 
-    deriv_spline_fit = interpolate.pchip_interpolate(
-        ankle_pts, deriv_pts, ankle_angle)
+    #LEFT
+    #ankle_pts = np.array([-67, -60, -47,-40, -20, -10 ,0, 10, 20, 30, 40, 45.6, 55, 80, 90, 100, 112])
+    #deriv_pts = np.array([14.85, 14, 11.89, 12.74, 14.28, 13.71, 12.54, 10.43, 8, 5.5, 2.3, 0.4, -3.3, -10, -11.30, -10.95, -8.75])
 
-    plt.plot(ankle_angle, deriv_spline_fit, linewidth=5,  label = "pchip manual")
+    #DEFAULT
+    #ankle_pts = np.array([-40, -20, 0, 10, 20, 30, 40, 45.6, 50, 55])  # Deg
+    #deriv_pts = np.array([19, 17, 16.5, 15.5, 13.5, 10, 4, -1, -5, -11 ])
+
+    deriv_spline_fit = interpolate.pchip_interpolate(ankle_pts, deriv_pts,
+                                                     ankle_angle)
+
+    plt.plot(ankle_angle, deriv_spline_fit, linewidth=5, label="pchip manual")
     plt.legend()
     plt.ylabel('Transmission Ratio')
     plt.xlabel('Ankle Angle')
 
-
-
-    motor_torque = constants.MAX_ALLOWABLE_CURRENT_COMMAND*constants.MOTOR_CURRENT_TO_MOTOR_TORQUE
+    motor_torque = constants.MAX_ALLOWABLE_CURRENT_COMMAND * constants.MOTOR_CURRENT_TO_MOTOR_TORQUE
 
     ankle_torque = motor_torque * \
         np.polyval(p_deriv, ankle_angle[50])
-
-    
-
 
 plt.show()
